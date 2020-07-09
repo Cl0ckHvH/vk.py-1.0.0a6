7ccbf939f684bd0614570c01f45297c6ce58df70
Cl0ckHvH <62774712+Cl0ckHvH@users.noreply.github.com>
2020-07-10 01:06:27 +0300

    Add files via upload

---
+++ vk/methods/__init__.py
@@ -0,0 +1,17 @@
+from .messages import Messages
+from .account import Account
+from .apps import Apps
+from .appwidgets import AppWidgets
+from .auth import Auth
+from .board import Board
+from .database import Database
+from .docs import Docs
+from .fave import Fave
+from .friends import Friends
+from .gifts import Gifts
+from .groups import Groups
+from .leads import Leads
+from .likes import Likes
+from .market import Market
+
+from .api import API
